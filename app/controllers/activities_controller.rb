d10ef7bf5cbc7870e6d5fccf50627e65c6ec557b
ameliarider <mimirider89@gmail.com>
2025-08-05 18:04:43 -0700

    Activities CRUD actions

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,47 @@
+class ActivitiesController < ApplicationController
+  def index
+    @activities = Activity.all
+
+    render json: @activities
+  end
+
+  def show
+    @activity = Activity.find_by(id: params["id"])
+
+    render json: @activity
+  end
+
+  def create
+    @activity = Activity.create!(
+      name: params["name"],
+      date: params["date"],
+      link: params["link"],
+      est_cost: params["est_cost"],
+      trip_id: params["trip_id"]
+    )
+
+    render json: @activity
+  end
+
+  def update
+    @activity = Activity.find_by(id: params["id"])
+
+    @activity.update(
+      name: params["name"] || @activity.name,
+      date: params["date"] || @activity.date,
+      link: params["link"] || @activity.link,
+      est_cost: params["est_cost"] || @activity.est_cost,
+      trip_id: params["trip_id"] || @activity.trip_id
+    )
+
+    render json: @activity
+  end
+
+  def destroy
+    @activity = Activity.find_by(id: params["id"])
+
+    @activity.destroy
+
+    render json: { message: "Activity destroyed successfully" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,39 @@ Rails.application.routes.draw do
   post "/trips" => "trips#create"
   patch "/trips/:id" => "trips#update"
   delete "/trips/:id" => "trips#destroy"
+
+  # Activities
+  get "/activities" => "activities#index"
+  get "/activities/:id" => "activities#show"
+  post "/activities" => "activities#create"
+  patch "/activities/:id" => "activities#update"
+  delete "/activities/:id" => "activities#destroy"
+
+  # Hotels
+  get "/hotels" => "hotels#index"
+  get "/hotels/:id" => "hotels#show"
+  post "/hotels" => "hotels#create"
+  patch "/hotels/:id" => "hotels#update"
+  delete "/hotels/:id" => "hotels#destroy"
+
+  # Locations
+  get "/locations" => "locations#index"
+  get "/locations/:id" => "locations#show"
+  post "/locations" => "locations#create"
+  patch "/locations/:id" => "locations#update"
+  delete "/locations/:id" => "locations#destroy"
+
+  # Restaurants
+  get "/restaurants" => "restaurants#index"
+  get "/restaurants/:id" => "restaurants#show"
+  post "/restaurants" => "restaurants#create"
+  patch "/restaurants/:id" => "restaurants#update"
+  delete "/restaurants/:id" => "restaurants#destroy"
+
+  # Travels
+  get "/travels" => "travels#index"
+  get "/travels/:id" => "travels#show"
+  post "/travels" => "travels#create"
+  patch "/travels/:id" => "travels#update"
+  delete "/travels/:id" => "travels#destroy"
 end
